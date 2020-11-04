81d6541d5af0cae74591b4a7323b17c2304f1209
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-04 13:45:10 +0300

    added functionality to validate user during login

--- auth/views.py
+++ auth/views.py
@@ -10,7 +10,13 @@ from ..email import mail_message
 @auth.route('/login',methods=['GET','POST'])
 def login():
     login_form = LoginForm()
-    
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('rental_hub.index'))
+
+        flash('Invalid username or Password')
 
     title = "Rental login"
     return render_template('auth/login.html',login_form = login_form,title=title)
