20787f3bbf529baf57ec44dd065bab1b4809d851
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-04 13:31:57 +0300

    added view functions for upvote and downvote for the user to like or dislike

--- rental_hub/forms.py
+++ rental_hub/forms.py
@@ -3,3 +3,11 @@ from wtforms import SelectField,StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
 
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Edit your bio',validators = [Required()])
+    submit = SubmitField('Edit bio')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Enter your comment')
+    submit = SubmitField('Comment')   
