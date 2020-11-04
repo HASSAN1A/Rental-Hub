5c01f95618cf75c128f7c4072879954a33e718f7
LekamCharity.gmail.com <LekamCharity.gmail.com>
2020-11-04 17:48:27 +0300

    initialized bootstrap

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+
+bootstrap = Bootstrap()
+
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+photos = UploadSet('photos',IMAGES)
+
+mail = Mail()
